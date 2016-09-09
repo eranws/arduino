f3c5e2826e29e92c1209883ca94fe7acf983a008
Eran <eranws@gmail.com>
2016-09-09 12:55:05 -0400

    set digital pin default to HIGH, to use with relay

--- examples/StandardFirmata/StandardFirmata.ino
+++ examples/StandardFirmata/StandardFirmata.ino
@@ -289,7 +289,7 @@ void setPinModeCallback(byte pin, int mode)
       break;
     case OUTPUT:
       if (IS_PIN_DIGITAL(pin)) {
-        digitalWrite(PIN_TO_DIGITAL(pin), LOW); // disable PWM
+        digitalWrite(PIN_TO_DIGITAL(pin), HIGH); // disable PWM
         pinMode(PIN_TO_DIGITAL(pin), OUTPUT);
         Firmata.setPinMode(pin, OUTPUT);
       }
